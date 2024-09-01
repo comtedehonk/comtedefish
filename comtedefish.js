86cf5d25df5a12fc08db2b17e80ecb8300991b51
Ryan <ryan7rcl@gmail.com>
2024-08-31 17:25:34 -0700

    wip

--- comtedefish.js
+++ comtedefish.js
@@ -1,6 +1,6 @@
 /* Pieces: 
 0: None, 1: White Pawn, 2: White Knight, 3: White Bishop, 4: White Rook, 5: White Queen, 6: White King, 
-7: Black Pawn, 8: Black Knight, 9: Black Bishop, 10: Black Rook, 11: Black Queen, 12: Black King
+9: Black Pawn, 10: Black Knight, 11: Black Bishop, 12: Black Rook, 13: Black Queen, 14: Black King
 */ 
 
 const edges = [
@@ -12,282 +12,82 @@ const edges = [
     0, 1, null, null, null, null, 2 ,3,
     0, 1, null, null, null, null, 2 ,3,
     0, 1, null, null, null, null, 2 ,3
-    ]
+    ];
 
+const piece = {
+    white: 8, // 1000
+    black: 16, // 10000
+    pawn: 1,  // 0001
+    knight: 2,// 0010
+    bishop: 3,// 0011
+    rook: 4,  // 0100
+    queen: 5, // 0101
+    king: 6   // 0110
+}
+
+
+class BitBoard {
+    constructor(int){
+        this.int = int[0];
+    }
+    holds(index){
+        this.int >>> index;
+        return Boolean(this.int >>> index & 1)
+    }
+    add(index){
+        let num = 1n << index;
+        this.int = this.int | num;
+    }
+    forEach1(func){
+        let num = this.int;
+        while (num !== 0){
+            func(index);
+            num &= (num - 1n);
+        }
+    }
+}
+
+class Square {
+    constructor(piece){
+        this.piece = piece;
+        this.pinned;
+        this.block;
+        this.EPblock;
+        this.controlled;
+    }
+}
 export class Position {
     constructor(currentBoard, castleRights, enPassantSquare, toMove, pieces){
-        this.currentBoard = currentBoard;
+        this.board = currentBoard;
         this.castleRights = castleRights;  // white kingside, white queenside, black kingside, black queenside
         this.enPassantSquare = enPassantSquare;
-        this.toMove = toMove;
-        this.pieces = pieces || this.getPiecePositions();        
-        if (this.toMove === "w"){
-            this.enemyPieces = this.pieces.black;
-            this.friendlyPieces = this.pieces.white;
-            this.isEnemy = function(squareValue){
-                if (squareValue > 6){
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-            this.isFriendly = function(squareValue){
-                if (squareValue < 7 && squareValue !== 0){
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-            this.onSecondRank = function(square){
-                if (square > 47 && square < 56){
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-            this.addPawnMove = function(list, originalSquare, newSquare){
-                if (originalSquare > 7 && originalSquare < 16){
-                    list.push([originalSquare, newSquare, 5]);
-                    list.push([originalSquare, newSquare, 4]);
-                    list.push([originalSquare, newSquare, 3]);
-                    list.push([originalSquare, newSquare, 2]);
-                } else {
-                    list.push([originalSquare, newSquare]);
-                }
-            }
-            this.makeMove = function(originalSquare, newSquare, promotionPiece){
-                if (typeof originalSquare === "number"){
-                    let newBoard = [...this.currentBoard];
-                    newBoard[originalSquare] = 0;
-                    newBoard[newSquare] = this.currentBoard[originalSquare];
-                    let enPassantSquare = null;
-                    let castleRights = this.castleRights;
-                    if (this.currentBoard[originalSquare] === 1){
-                        if (originalSquare - newSquare === 16){
-                            if ((this.currentBoard[newSquare - 1] === 7 && edges[newSquare - 1] !== 0) ||
-                            (this.currentBoard[newSquare + 1] === 7 && edges[newSquare + 1] !== 3)){
-                            enPassantSquare = newSquare + 8;
-                            }
-                        } else if (newSquare === this.enPassantSquare){
-                            newBoard[newSquare + 8] = 0;
-                        } else if (originalSquare > 7 && originalSquare < 16){
-                            newBoard[newSquare] = promotionPiece;
-                        }
-                    } else if (this.friendlyPieces.king === originalSquare){
-                        castleRights[0] = false;
-                        castleRights[1] = false;
-                    } else if (this.currentBoard[originalSquare] === 4){
-                        if (originalSquare === 56){
-                            castleRights[1] = false;
-                        } else if (originalSquare === 63){
-                            castleRights[0] = false;
-                        }
-                    }
-                    return new Position(newBoard, castleRights, enPassantSquare, "b");
-                } else {
-                    let castleRights = this.castleRights;
-                    let newBoard = [...this.currentBoard];
-                    newBoard[60] = 0;
-                    castleRights[0] = false;
-                    castleRights[1] = false;
-                    if (originalSquare === "O-O"){
-                        newBoard[63] = 0;
-                        newBoard[62] = 6;
-                        newBoard[61] = 4;
-                    } else {
-                        newBoard[56] = 0;
-                        newBoard[58] = 6;
-                        newBoard[59] = 4;
-                    }
-                    return new Position(newBoard, castleRights, null, "b");
-                }               
-            }
-        } else {
-            this.enemyPieces = this.pieces.white;
-            this.friendlyPieces = this.pieces.black;
-            this.isEnemy = function(squareValue){
-                if (squareValue < 7 && squareValue !== 0){
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-            this.isFriendly = function(squareValue){
-                if (squareValue > 6){
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-            this.onSecondRank = function(square){
-                if (square > 7 && square < 16){
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-            this.addPawnMove = function(list, originalSquare, newSquare){
-                if (originalSquare > 47 && originalSquare < 56){
-                    list.push([originalSquare, newSquare, 11]);
-                    list.push([originalSquare, newSquare, 10]);
-                    list.push([originalSquare, newSquare, 9]);
-                    list.push([originalSquare, newSquare, 8]);
-                } else {
-                    list.push([originalSquare, newSquare]);
-                }
-            }
-            this.makeMove = function(originalSquare, newSquare, promotionPiece){
-                if (typeof originalSquare === "number"){
-                    let newBoard = [...this.currentBoard];
-                    newBoard[originalSquare] = 0;
-                    newBoard[newSquare] = this.currentBoard[originalSquare];
-                    let enPassantSquare = null;
-                    let castleRights = this.castleRights;
-                    if (this.currentBoard[originalSquare] === 7){
-                        if (newSquare - originalSquare === 16){
-                            if ((this.currentBoard[newSquare - 1] === 1 && edges[newSquare - 1] !== 0) ||
-                            (this.currentBoard[newSquare + 1] === 1 && edges[newSquare + 1] !== 3)){
-                            enPassantSquare = newSquare - 8;
-                            }
-                        } else if (newSquare === this.enPassantSquare){
-                            newBoard[newSquare - 8] = 0;
-                        } else if (originalSquare > 47 && originalSquare < 56){
-                            newBoard[newSquare] = promotionPiece;
-                        }
-                    } else if (this.friendlyPieces.king === originalSquare){
-                        castleRights[2] = false;
-                        castleRights[3] = false;
-                    } else if (this.currentBoard[originalSquare] === 10){
-                        if (originalSquare === 0){
-                            castleRights[3] = false;
-                        } else if (originalSquare === 7){
-                            castleRights[2] = false;
-                        }
-                    }
-                    return new Position(newBoard, castleRights, enPassantSquare, "w");
-                } else {
-                    let castleRights = this.castleRights;
-                    let newBoard = [...this.currentBoard];
-                    newBoard[4] = 0;
-                    castleRights[2] = false;
-                    castleRights[3] = false;
-                    if (originalSquare === "O-O"){
-                        newBoard[7] = 0;
-                        newBoard[6] = 12;
-                        newBoard[4] = 10;
-                    } else {
-                        newBoard[0] = 0;
-                        newBoard[2] = 12;
-                        newBoard[3] = 10;
-                    }
-                    return new Position(newBoard, castleRights, null, "w");
-                }               
-            }
-        }
+        this.toMove = toMove;       
         this.legalMoves = this.calculateLegalMoves();
     }
-    getPiecePositions(){
-        let piecePositions = {
-            white: {
-                pawns: [],
-                knights: [],
-                bishops: [],
-                rooks: [],
-                queens: [],
-                king: 0
-            },
-            black : {
-                pawns: [],
-                knights: [],
-                bishops: [],
-                rooks: [],
-                queens: [],
-                king: 0
-            }
-        }
-        for (let i = 0; i < 64; i++){
-            switch (this.currentBoard[i]){
-                case 1:
-                    piecePositions.white.pawns.push(i);
-                    break;
-                case 2: 
-                    piecePositions.white.knights.push(i);
-                    break;
-                case 3:
-                    piecePositions.white.bishops.push(i);
-                    break;
-                case 4: 
-                    piecePositions.white.rooks.push(i);
-                    break;
-                case 5:
-                    piecePositions.white.queens.push(i);
-                    break;
-                case 6: 
-                    piecePositions.white.king = i;
-                    break;
-                case 7:
-                    piecePositions.black.pawns.push(i);
-                    break;
-                case 8: 
-                    piecePositions.black.knights.push(i);
-                    break; 
-                case 9:
-                    piecePositions.black.bishops.push(i);
-                    break;
-                case 10: 
-                    piecePositions.black.rooks.push(i);
-                    break;
-                case 11:
-                    piecePositions.black.queens.push(i);
-                    break;
-                case 12: 
-                    piecePositions.black.king = i;
-                    break;          
-            }
-        }
-        return piecePositions;
-    }
+    
 
 
     calculateLegalMoves(){
-    
         let legalMoves = [];
         let inCheck = false;
         let inDoubleCheck = false;
-        let blockSquares = [];
-        let oppControlledSquares = [ // index of piece controlling that square (stores as -1 if multiple)
-            null, null, null, null, null, null, null, null,
-            null, null, null, null, null, null, null, null,
-            null, null, null, null, null, null, null, null,
-            null, null, null, null, null, null, null, null,
-            null, null, null, null, null, null, null, null,
-            null, null, null, null, null, null, null, null,
-            null, null, null, null, null, null, null, null,
-            null, null, null, null, null, null, null, null,
-        ];
-        
-        const addControlledSquare = (square, homeSquare) => {
-            if (square === this.friendlyPieces.king){
+        let oppControlledSquares = new BitBoard(0n);
+        let blockSquares = new BitBoard(0n);
+        let enPassantBlockSquare = null;
+        const addPawnControlledSquare = (square, homeSquare) => {
+            if (this.board[square].piece === (this.friendly | piece.king)){
                 if (inCheck){
                     inDoubleCheck = true;
                 } else {
                     inCheck = true;
-                    blockSquares[homeSquare] = 1;
-                    let squareOffset = (this.toMove = "w") ? 8 : -8
+                    this.board[homeSquare].block = true;
+                    let squareOffset = (this.toMove = "w") ? 8 : -8;
                     if (this.enPassantSquare = homeSquare - squareOffset){
-                        blockSquares[homeSquare - squareOffset] = 2;
+                        this.board[homeSquare - squareOffset].EPblock = true;
                     }
                 }
             } else {
-                oppControlledSquares[square] = 1;
-            }
-        }
-
-        let pinnedPieces = [];
-        class PinnedPiece {
-            constructor(square, direction) {
-                this.square = square;
-                this.direction = direction;
+                this.board[square].controlled = true;
             }
         }
 
@@ -309,38 +109,38 @@ export class Position {
                     }
                     
                     while (edges[currentSquare] !== edge){
-                        if (this.currentBoard[currentSquare + i] === 0){
-                            oppControlledSquares[currentSquare + i] = 1;
+                        if (this.board[currentSquare + i].piece === 0){
+                            this.board[currentSquare + i].controlled = true;
                             currentSquare += i;   
-                        } else if (this.isFriendly(this.currentBoard[currentSquare + i])){
-                            if (this.friendlyPieces.king === currentSquare + i){
+                        } else if (this.board[currentSquare + i].piece & this.friendly){
+                            if (this.board[currentSquare + i].piece === this.friendly | piece.king){
                                 if (inCheck){
                                     inDoubleCheck = true;
                                 } else {
                                     inCheck = true;
                                     while (currentSquare !== homeSquare - i){
-                                        blockSquares[currentSquare] = 1;
+                                        this.board[currentSquare].block = true;
                                         currentSquare -= i;
                                     }
                                 }
                                 break;
                             }
-                            oppControlledSquares[currentSquare + i] = 1;
+                            this.board[currentSquare + i].controlled = true;
                             let potentialPinnedPiece = currentSquare + i;
                             currentSquare += i;
                             while (edges[currentSquare] !== edge){
-                                if (this.currentBoard[currentSquare + i] === 0){
+                                if (this.board[currentSquare + i].piece === 0){
                                     currentSquare += i;
-                                } else if (currentSquare + i === this.friendlyPieces.king){ // find pinned pieces
-                                    pinnedPieces.push(new PinnedPiece(potentialPinnedPiece, i))                     
+                                } else if (this.board[currentSquare + i].piece === this.friendly | piece.king){ // find pinned pieces
+                                    this.board[potentialPinnedPiece].pinned = i;                     
                                     break;
                                 } else {
                                     break;
                                 }
                             }
                             break;
-                        } else if (this.isEnemy(this.currentBoard[currentSquare + i])){
-                            oppControlledSquares[currentSquare + i] = 1;
+                        } else if (this.board[currentSquare + i].piece & this.enemy){
+                            this.board[currentSquare + i].controlled = true;
                             break;
                         } else {
                             break;
@@ -351,12 +151,12 @@ export class Position {
         for (let i of this.enemyPieces.pawns){            
             let directions = (this.toMove === "b") ? [i - 7, i - 9] : [i + 9, i + 7];                    
             if (edges[i] === 0){
-                addControlledSquare(directions[0], i);
+                addPawnControlledSquare(directions[0], i);
             } else if (edges[i] === 3){
-                addControlledSquare(directions[1], i);
+                addPawnControlledSquare(directions[1], i);
             } else {
-                addControlledSquare(directions[0], i);
-                addControlledSquare(directions[1], i);
+                addPawnControlledSquare(directions[0], i);
+                addPawnControlledSquare(directions[1], i);
             }
         }
         for (let i of this.enemyPieces.knights){
@@ -759,7 +559,7 @@ export class Position {
 }
 
 
-export let currentPosition = new Position([
+export let currentPosition = new Position(new Uint8Array([
     10,8, 9, 11,0,12, 0, 10,
     7, 7, 0, 1, 9, 7, 7, 7,
     0, 0, 7, 0, 0, 0, 0, 0,
@@ -768,7 +568,7 @@ export let currentPosition = new Position([
     0, 0, 0, 0, 0, 0, 0, 0,
     1, 1, 1, 0, 2, 8, 1, 1,
     4, 2, 3, 5, 6, 0, 0, 4
-], [true, true, false, false], null, "w");
+]), [true, true, false, false], null, "w");
 
 /* original position:
 10,8, 9, 11,12,9, 8, 10,
@@ -779,6 +579,9 @@ export let currentPosition = new Position([
 0, 0, 0, 0, 0, 0, 0, 0,
 1, 1, 1, 1, 1, 1, 1, 1,
 4, 2, 3, 5, 6, 3, 2, 4 
+
+
+
 */
 
 
